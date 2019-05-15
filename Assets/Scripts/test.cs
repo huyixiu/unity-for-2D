e04c660f7b7b8c03b6ec6e84e848b1abd8e1cf73
LAPTOP-0HEC0C79\huyix <huyixiu0423@qq.com>
2019-05-15 09:54:59 +0800

    添加文件

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class test : MonoBehaviour {
+public class test : MonoBehaviour
+{
+    public Text TesText;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +16,11 @@ public class test : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+    public void testPrint()
+    {
+        print("hello world!");
+        TesText.text = "Hello World!";
+    }
 }
